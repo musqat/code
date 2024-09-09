eae507a58884f9b29a4930ea73e60f922a468a2e
musqat <50176358+musqat@users.noreply.github.com>
2024-09-09 23:08:21 +0900

    [Gold IV] Title: 팰린드롬?, Time: 588 ms, Memory: 230864 KB -BaekjoonHub

--- 백준/Gold/10942. 팰린드롬？/팰린드롬？.java
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.java
@@ -6,42 +6,56 @@ import java.util.StringTokenizer;
 public class Main {
     private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static boolean[][] dp;
+
     public static void main(String[] args) throws IOException {
-        int n = Integer.parseInt(br.readLine());
-        int []arr = new int[n + 1];
-        dp = new boolean[n + 1][n + 1];
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N + 1];
+        dp = new boolean[N + 1][N + 1];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i = 1 ; i <= n; i++)
-            arr[i] = Integer.parseInt(st.nextToken());
 
-        solve(arr, n);
-        int m = Integer.parseInt(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        pal(arr, N);
+
+        int M = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
 
-        for(int i = 0 ; i < m; i++){
+        for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
+            int S = Integer.parseInt(st.nextToken());
+            int E = Integer.parseInt(st.nextToken());
 
-            if(dp[start][end]) sb.append("1\n");
-            else sb.append("0\n");
+            if (dp[S][E]) {
+                sb.append("1\n");
+            } else {
+                sb.append("0\n");
+            }
         }
 
         System.out.println(sb);
     }
-    public static void solve(int[] arr, int n){
-        for(int i = 1; i <= n; i++)
+
+    public static void pal(int[] arr, int N) {
+        for (int i = 1; i <= N; i++) {
             dp[i][i] = true;
+        }
 
-        for(int i = 1; i <= n - 1; i++)
-            if(arr[i] == arr[i + 1]) dp[i][i + 1]= true;
+        for (int i = 1; i <= N - 1; i++) {
+            if (arr[i] == arr[i + 1]) {
+                dp[i][i + 1] = true;
+            }
+        }
 
-        for(int i = 2; i < n; i++){
-            for(int j = 1; j <= n - i; j++){
-                if(arr[j] == arr[j + i] && dp[j + 1][j + i - 1])
+        for (int i = 2; i <= N; i++) {
+            for (int j = 1; j <= N - i; j++) {
+                if (arr[j] == arr[j + i] && dp[j + 1][j + i - 1]) {
                     dp[j][j + i] = true;
+                }
             }
         }
     }
+
 }
