38071476983e459b015919e24b0626adf7e96ab0
musqat <50176358+musqat@users.noreply.github.com>
2024-10-12 10:40:11 +0900

    [Silver IV] Title: 카드2, Time: 116 ms, Memory: 43032 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        Queue<Integer> queue = new LinkedList<>();
+
+        for (int i = 1; i <= n; i++) {
+            queue.offer(i);
+        }
+
+        while (queue.size() > 1){
+            queue.poll();
+            queue.offer(queue.poll());
+        }
+
+        System.out.println(queue.poll());
+    }
+}
