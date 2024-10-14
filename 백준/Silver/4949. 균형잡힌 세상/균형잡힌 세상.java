b74a7192ebf00a26dd9bfb2870e282c7201ad7cb
musqat <50176358+musqat@users.noreply.github.com>
2024-10-14 14:44:24 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 228 ms, Memory: 17600 KB -BaekjoonHub

---
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input;
+
+        while (!(input = br.readLine()).equals(".")) {
+            if (isBalanced(input)) {
+                System.out.println("yes");
+            } else {
+                System.out.println("no");
+            }
+        }
+    }
+
+    private static boolean isBalanced(String str) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : str.toCharArray()) {
+            if (ch == '(' || ch == '[') {
+                stack.push(ch); 
+            } else if (ch == ')') {
+                if (stack.isEmpty() || stack.pop() != '(') {
+                    return false;
+                }
+            } else if (ch == ']') {
+                if (stack.isEmpty() || stack.pop() != '[') {
+                    return false; 
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
