3dee19e45245071ecbb7f4eee69716f9e7d05f32
musqat <50176358+musqat@users.noreply.github.com>
2024-10-25 11:48:38 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 64 ms, Memory: 11228 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        System.out.println("       _.-;;-._\n" +
+                "'-..-'|   ||   |\n" +
+                "'-..-'|_.-;;-._|\n" +
+                "'-..-'|   ||   |\n" +
+                "'-..-'|_.-''-._|\n");
+    }
+
+}
