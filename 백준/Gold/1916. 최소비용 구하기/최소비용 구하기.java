17fdae75ac63c1573f344dbbee8b2ba820c804a4
musqat <50176358+musqat@users.noreply.github.com>
2025-03-14 18:24:54 +0900

    [Gold V] Title: 최소비용 구하기, Time: 460 ms, Memory: 52648 KB -BaekjoonHub

---
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
@@ -0,0 +1,79 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    static class Node implements Comparable<Node>{
+        int to;
+        int weight;
+
+        public Node(int to, int weight) {
+            this.to = to;
+            this.weight = weight;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            return this.weight - o.weight;
+        }
+    }
+
+    public static void dijkstra(int v, List<List<Node>> graph, int start, int end) {
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        int[] dist = new int[v + 1];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[start] = 0;
+
+        pq.offer(new Node(start, 0));
+
+        while (!pq.isEmpty()){
+            Node curNode = pq.poll();
+            int cur = curNode.to;
+
+            if (curNode.weight > dist[cur]){
+                continue;
+            }
+            for(Node adj : graph.get(cur)){
+                if (dist[adj.to] > dist[cur] + adj.weight){
+                    dist[adj.to] = dist[cur] + adj.weight;
+                    pq.offer(new Node(adj.to, dist[adj.to]));
+                }
+            }
+        }
+
+            System.out.println(dist[end]);
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        Scanner sc = new Scanner(System.in);
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int V = Integer.parseInt(st.nextToken());
+        int E = Integer.parseInt(br.readLine());
+
+        List<List<Node>> graph = new ArrayList<>();
+        for (int i = 0; i <= V; i++) {
+            graph.add(new ArrayList<>());
+        }
+        for (int i = 0; i < E; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+            graph.get(u).add(new Node(v, w));
+        }
+
+        st = new StringTokenizer(br.readLine());
+        int start = Integer.parseInt(st.nextToken());
+        int end = Integer.parseInt(st.nextToken());
+
+        dijkstra(V, graph, start, end);
+
+    }
+
+}
