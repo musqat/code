7019ea301aa5f375635debec3e57898a59ffefd9
musqat <50176358+musqat@users.noreply.github.com>
2025-04-16 11:25:00 +0900

    [Bronze IV] Title: 기찍 N, Time: 1060 ms, Memory: 35892 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        Scanner sc = new Scanner(System.in);
+
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = n; i > 0; i--) {
+            System.out.println(i);
+
+        }
+    }
+
+}
