48aa85bce4e4efa88459a3657e6591ad91ee9b5b
musqat <50176358+musqat@users.noreply.github.com>
2025-04-17 19:48:07 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 64 ms, Memory: 11508 KB -BaekjoonHub

---
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        long[][] dp = new long[n + 1][10];
+
+        for (int i = 1; i <= 9; i++) {
+            dp[1][i] = 1;
+        }
+
+        for (int i = 2; i <= n; i++) {
+            for (int j = 0; j <= 9; j++) {
+                if (j > 0) {
+                    dp[i][j] += dp[i - 1][j - 1];
+                }
+                if (j < 9) {
+                    dp[i][j] += dp[i - 1][j + 1];
+                }
+                dp[i][j] %= 1_000_000_000;
+            }
+
+        }
+
+        long sum = 0;
+        for (int i = 0; i <= 9; i++) {
+            sum = (sum + dp[n][i]) % 1_000_000_000;
+
+        }
+
+        System.out.println(sum);
+
+
+    }
+}
