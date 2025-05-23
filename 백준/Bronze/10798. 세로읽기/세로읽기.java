a9e9519b750b5f8f2d7d2076db8642e5da7fafc1
musqat <50176358+musqat@users.noreply.github.com>
2025-05-24 03:35:45 +0900

    [Bronze I] Title: 세로읽기, Time: 64 ms, Memory: 11496 KB -BaekjoonHub

---
+++ 백준/Bronze/10798. 세로읽기/세로읽기.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        char[][] arr = new char[5][15];
+
+        for (int i = 0; i < 5; i++) {
+            String line = br.readLine();
+            for (int j = 0; j < line.length(); j++) {
+                arr[i][j] = line.charAt(j);
+
+            }
+        }
+        for (int i = 0; i < 15; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (arr[j][i] != '\0') {
+                    System.out.print(arr[j][i]);
+                }
+
+            }
+        }
+
+
+    }
+
+}
