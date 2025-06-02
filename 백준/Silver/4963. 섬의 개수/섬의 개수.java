805020dbb014ff37106048d067ebfb0e854947b3
musqat <50176358+musqat@users.noreply.github.com>
2025-06-02 17:54:45 +0900

    [Silver II] Title: 섬의 개수, Time: 96 ms, Memory: 13280 KB -BaekjoonHub

---
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.java
@@ -0,0 +1,63 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static int w, h;
+    static int[][] board;
+    static boolean[][] visited;
+    static int[] dx = {1, 0, -1, 0, 1, 1, -1, -1};
+    static int[] dy = {0, 1, 0, -1, 1, -1, 1, -1};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        while (true) {
+            st = new StringTokenizer(br.readLine());
+            w = Integer.parseInt(st.nextToken());
+            h = Integer.parseInt(st.nextToken());
+
+            if (w == 0 && h == 0) {
+                break;
+            }
+            board = new int[h][w];
+            visited = new boolean[h][w];
+
+            for (int i = 0; i < h; i++) {
+                st = new StringTokenizer(br.readLine());
+                for (int j = 0; j < w; j++) {
+                    board[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
+
+            int count = 0;
+            for (int i = 0; i < h; i++) {
+                for (int j = 0; j < w; j++) {
+                    if (board[i][j] == 1 && !visited[i][j]) {
+                        dfs(i, j);
+                        count++;
+                    }
+                }
+            }
+            System.out.println(count);
+        }
+
+
+    }
+
+
+    public static void dfs(int x, int y) {
+        visited[x][y] = true;
+        for (int i = 0; i < 8; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if (nx >= 0 && nx < h&& ny >= 0 && ny < w) {
+                if (board[nx][ny] == 1 && !visited[nx][ny]) {
+                    dfs(nx, ny);
+                }
+            }
+        }
+    }
+}
