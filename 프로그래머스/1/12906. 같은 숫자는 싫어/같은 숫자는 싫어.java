6ac4fa19fbc675621bd259dfe160a26ed67df190
musqat <50176358+musqat@users.noreply.github.com>
2025-06-06 13:32:46 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 23.53 ms, Memory: 113 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Solution {
+    public Object[] solution(int[] arr) {
+        List<Integer> answer = new ArrayList<>();
+        
+        answer.add(arr[0]);
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] != arr[i -1]){
+                answer.add(arr[i]);
+            }
+
+
+        }
+
+        return answer.toArray();
+    }
+}
