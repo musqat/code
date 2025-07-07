f49c12a8189f41d4231cbac99b2c758c53dd8220
musqat <50176358+musqat@users.noreply.github.com>
2025-07-07 14:55:58 +0900

    [Silver III] Title: 수열, Time: 992 ms, Memory: 23928 KB -BaekjoonHub

---
+++ 백준/Silver/2559. 수열/수열.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N];
+        int sum = Integer.MIN_VALUE;
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i <= N - K; i++) {
+            int tmp = 0;
+            for (int j = 0; j < K; j++) {
+                tmp += arr[i + j];
+            }
+            sum = Math.max(sum, tmp);
+        }
+
+        System.out.println(sum);
+    }
+}
