883e07f9eb470b1563658e70d81507c76bf53ee9
musqat <50176358+musqat@users.noreply.github.com>
2025-07-20 23:36:31 +0900

    [Bronze IV] Title: 과자, Time: 104 ms, Memory: 14216 KB -BaekjoonHub

---
+++ 백준/Bronze/10156. 과자/과자.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        int answer = c - (a * b);
+
+        System.out.println(answer <= 0 ? Math.abs(answer) : 0);
+    }
+}
