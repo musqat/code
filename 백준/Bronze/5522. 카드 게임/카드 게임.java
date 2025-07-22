7be3c95cf57afd0d7ce813703a00db37a8f3606c
musqat <50176358+musqat@users.noreply.github.com>
2025-07-22 15:00:04 +0900

    [Bronze V] Title: 카드 게임, Time: 100 ms, Memory: 14132 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/5522. 카드 게임/카드 게임.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int result = 0;
+
+        for (int i = 0; i < 5; i++) {
+            result += Integer.parseInt(br.readLine());
+        }
+        System.out.println(result);
+    }
+}
