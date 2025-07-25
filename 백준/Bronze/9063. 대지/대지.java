2b96ad4fd176765ac28e962c26df03a3369afb3e
musqat <50176358+musqat@users.noreply.github.com>
2025-07-25 19:31:10 +0900

    [Bronze III] Title: 대지, Time: 364 ms, Memory: 31628 KB -BaekjoonHub

---
+++ 백준/Bronze/9063. 대지/대지.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        if (N == 1) {
+            System.out.println(0);
+            return;
+        }
+
+        int[] x = new int[N];
+        int[] y = new int[N];
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            x[i] = Integer.parseInt(st.nextToken());
+            y[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(x);
+        Arrays.sort(y);
+
+        int xLen = x[N - 1] -  x[0];
+        int yLen = y[N - 1] -  y[0];
+
+        System.out.println(xLen * yLen);
+    }
+}
