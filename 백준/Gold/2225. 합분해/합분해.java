043eca6eed7bc10401483c417724084650820154
musqat <50176358+musqat@users.noreply.github.com>
2025-07-30 13:25:33 +0900

    [Gold V] Title: 합분해, Time: 108 ms, Memory: 14264 KB -BaekjoonHub

---
+++ 백준/Gold/2225. 합분해/합분해.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static final int MOD = 1000000000;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int[][] dp = new int[K + 1][N + 1];
+
+        for (int i = 0; i <=K; i++) {
+            dp[i][0] = 1;
+        }
+
+        for (int i = 1; i <= K; i++) {
+            for (int j = 1; j <= N; j++) {
+                dp[i][j] = (dp[i][j-1] + dp[i - 1][j]) % MOD;
+            }
+        }
+
+        System.out.println(dp[K][N]);
+    }
+}
+
