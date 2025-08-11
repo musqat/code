61556bee5bc3489bedeacb45aa9cb27e9b3d4807
musqat <50176358+musqat@users.noreply.github.com>
2025-08-11 21:06:40 +0900

    [Silver V] Title: 최소공배수, Time: 104 ms, Memory: 14164 KB -BaekjoonHub

---
+++ 백준/Silver/13241. 최소공배수/최소공배수.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        long a = Integer.parseInt(st.nextToken());
+        long b = Integer.parseInt(st.nextToken());
+
+
+        if (b % a == 0) System.out.println(b);
+        else System.out.println(lcm(a,b));
+    }
+
+    static long gcd(long a, long b){
+        if (b == 0) return a;
+        return gcd(b, a % b);
+    }
+
+    static long lcm(long a, long b){
+        return (a*b) / gcd(a, b);
+    }
+}
