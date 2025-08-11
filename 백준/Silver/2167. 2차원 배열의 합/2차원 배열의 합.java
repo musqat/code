2b92a03b783ca3fc791a9e31523e6981626809a9
musqat <50176358+musqat@users.noreply.github.com>
2025-08-11 21:15:40 +0900

    [Silver V] Title: 2차원 배열의 합, Time: 1480 ms, Memory: 48188 KB -BaekjoonHub

---
+++ 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[][] arr = new int[N][M];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int K = Integer.parseInt(br.readLine());
+
+        while (K-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+            int result = 0;
+            for (int i = x1 -1; i < x2; i++) {
+                for (int j = y1 - 1; j < y2; j++) {
+                    result += arr[i][j];
+                }
+            }
+            System.out.println(result);
+
+
+        }
+
+
+    }
+}
