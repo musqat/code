dbfe7de282f14e1a21f6dda8d971d9cd4ac930a8
musqat <50176358+musqat@users.noreply.github.com>
2025-08-13 15:52:39 +0900

    [Bronze V] Title: 아!, Time: 108 ms, Memory: 14176 KB -BaekjoonHub

---
+++ 백준/Bronze/4999. 아！/아！.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String A = br.readLine();
+        String B = br.readLine();
+
+        System.out.println(A.contains(B) ? "go" : "no");
+
+
+    }
+}
