760188346883155dde6cfe7af8d0a5350c1bc343
musqat <50176358+musqat@users.noreply.github.com>
2025-08-18 16:24:25 +0900

    [Bronze V] Title: 나부 함대 데이터, Time: 96 ms, Memory: 13988 KB -BaekjoonHub

---
+++ 백준/Bronze/9654. 나부 함대 데이터/나부 함대 데이터.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+        System.out.println("SHIP NAME      CLASS          DEPLOYMENT IN SERVICE\n" +
+                "N2 Bomber      Heavy Fighter  Limited    21        \n" +
+                "J-Type 327     Light Combat   Unlimited  1         \n" +
+                "NX Cruiser     Medium Fighter Limited    18        \n" +
+                "N1 Starfighter Medium Fighter Unlimited  25        \n" +
+                "Royal Cruiser  Light Combat   Limited    4         \n");
+
+
+    }
+}
