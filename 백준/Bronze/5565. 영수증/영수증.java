0ea097a38ee32406f07add0bc11c35eb6bdc5a4a
musqat <50176358+musqat@users.noreply.github.com>
2025-08-21 16:50:03 +0900

    [Bronze IV] Title: 영수증, Time: 104 ms, Memory: 14188 KB -BaekjoonHub

---
+++ 백준/Bronze/5565. 영수증/영수증.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+
+
+        for (int i = 0; i < 9; i++) {
+            N -= Integer.parseInt(br.readLine());
+        }
+
+        System.out.println(N);
+
+    }
+}
