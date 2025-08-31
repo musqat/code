5e4f266cafd4fb8eb39d5fd67aca3bb91592e884
musqat <50176358+musqat@users.noreply.github.com>
2025-08-31 21:45:38 +0900

    [Gold IV] Title: 좋다, Time: 160 ms, Memory: 14840 KB -BaekjoonHub

---
+++ 백준/Gold/1253. 좋다/좋다.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        int count = 0;
+
+        for (int i = 0; i < N; i++) {
+            int target = arr[i];
+            int left = 0, right = N - 1;
+            while (left < right) {
+                if (left == i) {
+                    left++;
+                    continue;
+                }
+                if (right == i) {
+                    right--;
+                    continue;
+                }
+
+                int sum = arr[left] + arr[right];
+
+                if (sum == target) {
+                    count++;
+                    break;
+                } else if (sum < target) {
+                    left++;
+                } else {
+                    right--;
+
+                }
+            }
+        }
+        System.out.println(count);
+    }
+}
