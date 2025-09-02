aef2def69213543bcdb2892a75f935610b55a4f9
musqat <50176358+musqat@users.noreply.github.com>
2025-09-02 11:51:05 +0900

    [Silver V] Title: 행렬 곱셈, Time: 164 ms, Memory: 16072 KB -BaekjoonHub

---
+++ 백준/Silver/2740. 행렬 곱셈/행렬 곱셈.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[][] A = new int[N][M];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                A[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        st = new StringTokenizer(br.readLine());
+        M = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int[][] B = new int[M][K];
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < K; j++) {
+                B[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int[][] C = new int[N][K];
+
+        for (int i = 0; i < N; i++) {
+            for (int t = 0; t < M; t++) {
+                int a = A[i][t];
+                if (a == 0)continue;
+                for (int j = 0; j < K; j++) {
+                    C[i][j] += a * B[t][j];
+                }
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < K; j++) {
+                if (j > 0) sb.append(' ');
+                sb.append(C[i][j]);
+            }
+            sb.append('\n');
+        }
+
+        System.out.println(sb);
+    }
+}
