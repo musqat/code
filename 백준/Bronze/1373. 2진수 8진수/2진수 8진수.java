271200d41d55a55ce262ae7e02f3603fb78ab1ac
musqat <50176358+musqat@users.noreply.github.com>
2025-09-09 22:48:24 +0900

    [Bronze I] Title: 2진수 8진수, Time: 1936 ms, Memory: 90660 KB -BaekjoonHub

---
+++ 백준/Bronze/1373. 2진수 8진수/2진수 8진수.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        BigInteger a = new BigInteger(s, 2);
+
+        System.out.println(a.toString(8));
+    }
+}
