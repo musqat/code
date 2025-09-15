4c85dcb2636f95e0c9ef32acbe0338a6c58e56c2
musqat <50176358+musqat@users.noreply.github.com>
2025-09-15 21:22:08 +0900

    [Bronze III] Title: 공, Time: 104 ms, Memory: 14192 KB -BaekjoonHub

---
+++ 백준/Bronze/1547. 공/공.java
@@ -0,0 +1,22 @@
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int ball = 1;
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            if (ball == a) ball = b;
+            else if (ball == b) ball= a;
+        }
+
+
+        System.out.println(ball);
+    }
+}
