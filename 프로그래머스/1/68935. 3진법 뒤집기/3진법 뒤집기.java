d7ebaa3cc98ccbe0eaa1de218129181d4ba7d77e
musqat <50176358+musqat@users.noreply.github.com>
2025-11-19 12:42:32 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.05 ms, Memory: 79 MB -BaekjoonHub

---
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(int n) {
+
+        String reversed = new StringBuilder(Integer.toString(n, 3)).reverse().toString();
+
+        return Integer.parseInt(reversed, 3);
+    }
+}
