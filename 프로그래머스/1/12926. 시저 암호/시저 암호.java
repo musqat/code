3444cef39195fdd84746ac5a54306bec8e14e9ca
musqat <50176358+musqat@users.noreply.github.com>
2025-11-28 18:41:03 +0900

    [level 1] Title: 시저 암호, Time: 2.40 ms, Memory: 89.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(String s, int n) {
+        StringBuilder sb = new StringBuilder();
+
+        for (char c : s.toCharArray()) {
+            if (Character.isUpperCase(c)) {
+                sb.append((char) ((c - 'A' + n) % 26 + 'A'));
+            } else if (Character.isLowerCase(c)) {
+                sb.append((char) ((c - 'a' + n) % 26 + 'a'));
+            } else {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+}
