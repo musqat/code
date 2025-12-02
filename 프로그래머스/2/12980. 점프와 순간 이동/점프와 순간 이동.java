97c3e585a037d150ac152d6423df9f46715f61b6
musqat <50176358+musqat@users.noreply.github.com>
2025-12-02 21:11:56 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.05 ms, Memory: 54 MB -BaekjoonHub

---
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+        int ans = 0;
+        String s = Integer.toBinaryString(n);
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '1'){
+                ans++;
+            }
+
+        }
+
+        return ans;
+    }
+}
