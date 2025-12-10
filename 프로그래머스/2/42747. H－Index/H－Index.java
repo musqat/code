e4de53c82187b5f29bdf266a02583309b43754f9
musqat <50176358+musqat@users.noreply.github.com>
2025-12-10 20:30:00 +0900

    [level 2] Title: H-Index, Time: 0.85 ms, Memory: 72.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] citations) {
+        int answer = 0;
+        int n = citations.length;
+        Arrays.sort(citations);
+
+        for (int i = 0; i < n; i++) {
+            if (citations[i] >= n - i){
+                return n- i;
+            }
+            
+        }
+        
+        return 0;
+    }
+}
