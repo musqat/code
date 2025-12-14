bf993a6d9731ef0bcf2df45d9f57def6ecb59261
musqat <50176358+musqat@users.noreply.github.com>
2025-12-15 01:32:19 +0900

    [level 2] Title: 기능개발, Time: 3.62 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -2,38 +2,30 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        int[] answer;
         int len = progresses.length;
-
         List<Integer> days = new ArrayList<>();
-
-
+        List<Integer> list = new ArrayList<>();
+        
         for (int i = 0; i < len; i++) {
             int day = (int) Math.ceil((100.0 - progresses[i]) / speeds[i]);
             days.add(day);
         }
-
-        List<Integer> list = new ArrayList<>();
         
         int base = days.get(0);
         int count = 1;
 
         for (int i = 1; i < len; i++) {
-            if (days.get(i) <= base){
+            if (days.get(i) <= base) {
                 count++;
-            }else {
+            } else {
                 list.add(count);
                 base = days.get(i);
                 count = 1;
             }
         }
         list.add(count);
-        
-        
-        answer = new int[list.size()];
-        for (int i = 0; i < list.size(); i++) {
-            answer[i] = list.get(i);
-        }
-        return answer;
+
+
+        return list.stream().mapToInt(i -> i).toArray();
     }
 }
