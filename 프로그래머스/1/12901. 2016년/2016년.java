9ca247f40cefe4726cc1132358b41a9538597138
musqat <50176358+musqat@users.noreply.github.com>
2025-12-25 07:55:56 +0900

    [level 1] Title: 2016년, Time: 1.45 ms, Memory: 81.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12901. 2016년/2016년.java
@@ -0,0 +1,11 @@
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+
+class Solution {
+    public String solution(int a, int b) {
+        return LocalDate.of(2016,a,b)
+                .getDayOfWeek()
+                .toString()
+                .substring(0,3);
+    }
+}
