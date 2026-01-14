bfbdb9d333b0c732e43fe8e82491b6242875b483
musqat <50176358+musqat@users.noreply.github.com>
2026-01-14 12:13:23 +0900

    [level 1] Title: 소수 찾기, Time: 104.81 ms, Memory: 54.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+
+        for (int i = 2; i <= n; i++) {
+            if (isPrime(i)){
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+
+
+    private boolean isPrime(int n){
+        if (n < 2) return false;
+        if (n == 2) return true;
+        if (n % 2 == 0) return false;
+
+        for (int i = 3; i*i<=n ; i+=2) {
+            if (n % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
