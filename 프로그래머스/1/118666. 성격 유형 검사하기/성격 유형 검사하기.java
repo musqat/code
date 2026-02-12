e630eed1fec20077bd863af6bf807a51052e6fa8
musqat <50176358+musqat@users.noreply.github.com>
2026-02-13 04:36:59 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 12.59 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
@@ -1,27 +1,6 @@
-import java.util.HashMap;
-import java.util.Map;
-
 class Solution {
     public String solution(String[] survey, int[] choices) {
         String answer = "";
-        Map<String, Integer> map = new HashMap<>();
-        String[][] types = {{"R", "T"}, {"C", "F"}, {"J", "M"}, {"A", "N"}};
-
-        for (int i = 0; i < survey.length; i++) {
-            String[] str = survey[i].split("");
-            int score = choices[i] - 4;
-            if (score < 0) {
-                map.merge(str[0], -score, Integer::sum);
-            } else if (score > 0) {
-                map.merge(str[1], score, Integer::sum);
-            }
-        }
-
-        for (String[] pair : types){
-            int left = map.getOrDefault(pair[0],0);
-            int right = map.getOrDefault(pair[1],0);
-            answer += (left >= right) ? pair[0] : pair[1];
-        }
         return answer;
     }
 }
