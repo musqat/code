ab2f2449034f9505032647aed552e8a218725ab2
musqat <50176358+musqat@users.noreply.github.com>
2026-02-21 22:49:46 +0900

    [Bronze III] Title: 이진수, Time: 84 ms, Memory: 13984 KB -BaekjoonHub

---
+++ 백준/Bronze/3460. 이진수/이진수.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        while (T-- > 0) {
+            StringBuilder sb = new StringBuilder();
+            String v = Integer.toBinaryString(Integer.parseInt(br.readLine()));
+
+            for (int i = v.length() - 1; i >= 0; i--) {
+                if (v.charAt(i) == '1') {
+                    sb.append(v.length() - 1 - i).append(' ');
+                }
+            }
+            System.out.println(sb.toString());
+        }
+
+    }
+}
