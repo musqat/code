03e0893bce38d6a463bb904f998499ce1b4844e6
musqat <50176358+musqat@users.noreply.github.com>
2026-02-24 14:25:06 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 116 ms, Memory: 15884 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        String[][] arr = new String[3][3];
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                arr[i][j] = ":fan:";
+            }
+        }
+
+        arr[1][1] = ":" + s + ":";
+
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(arr[i][j]);
+            }
+            System.out.println();
+        }
+
+    }
+
+
+
+
+}
