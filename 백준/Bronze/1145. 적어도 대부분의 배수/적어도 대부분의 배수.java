443c783ccbc06f3a1d373f4363b844118f57b1fd
musqat <50176358+musqat@users.noreply.github.com>
2026-03-31 20:44:08 +0900

    [Bronze I] Title: 적어도 대부분의 배수, Time: 116 ms, Memory: 14428 KB -BaekjoonHub

---
+++ 백준/Bronze/1145. 적어도 대부분의 배수/적어도 대부분의 배수.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[5];
+        for (int i = 0; i < 5; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        long result = Long.MAX_VALUE;
+        for (int i = 0; i < 3; i++) {
+            for (int j = i + 1; j < 4; j++) {
+                for (int k = j + 1; k < 5; k++) {
+                    long l = lcm(lcm(arr[i], arr[j]), arr[k]);
+                    result = Math.min(result, l);
+                }
+            }
+        }
+        System.out.println(result);
+    }
+
+
+    static long gcd(long a, long b) {
+        while (b != 0) {
+            long t = b;
+            b = a % b;
+            a = t;
+        }
+        return a;
+    }
+
+    static long lcm(long a, long b) {
+        return a / gcd(a, b) * b;
+    }
+}
