e1729673102c3cf9a9d9fbd227710563cad8ef3f
musqat <50176358+musqat@users.noreply.github.com>
2026-05-06 21:53:47 +0900

    Time: 33 ms (0.12%), Space: 19.2 MB (52.46%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -0,0 +1,11 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n == 0 or n == 1 :
+            return 1
+        prev, cur = 1, 1
+        for i in range(2, n + 1) :
+            tmp = cur
+            cur = prev + cur
+            prev = tmp
+        return cur
+        
