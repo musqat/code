1e85513eafc3875aa23eb56f9459d4bd7ad38e1e
마스카트 <50176358+musqat@users.noreply.github.com>
2026-08-23 00:49:32 +0900

    Time: 0 ms (100%), Space: 42.5 MB (9.39%) - LeetHub

---
+++ 0476-number-complement/0476-number-complement.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int findComplement(int num) {
+        if(num == 0) return 1;
+
+        int bitlen = Integer.toBinaryString(num).length();
+
+        int mask = (1 << bitlen) - 1;
+        
+        return num ^ mask;
+    }
+}
